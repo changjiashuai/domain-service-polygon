a5642e63d1b2177422b65c076f7ef48f5299f769
changjiashuai <changjiashuai@gmail.com>
2022-10-10 18:03:39 +0800

    add connect wallet button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,31 +8,85 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-  return (
-		<div className="App">
-			<div className="container">
+    const [currentAccount, setCurrentAccount] = useState("");
 
-				<div className="header-container">
-					<header>
-            <div className="left">
-              <p className="title">🐱‍👤 Changjiashuai Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
+    const connectWallet = async () => {
+        try {
+            const {ethereum} = window;
+            if (!ethereum){
+                alert("Get MetaMask -> https://metamask.io/");
+                return;
+            }
+
+            const accounts = await ethereum.request({method:"eth_requestAccounts"});
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    const checkIfWalletIsConnected = async () => {
+        const {ethereum} = window;
+        if (!ethereum) {
+            console.log("Make sure you have MetaMask!");
+            return;
+        } else {
+            console.log("We have the ethereum object", ethereum);
+        }
+
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+        if (accounts.length !== 0){
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            setCurrentAccount(account);
+        }else {
+            console.log("No authorized account found");
+        }
+    }
+
+    const renderNotConnectedContainer = () => (
+        <div className="connect-wallet-container">
+            <img
+                src="https://media0.giphy.com/media/JRlqKEzTDKci5JPcaL/100.webp?cid=ecf05e47n18rvngurwpvfplpns82tjp2u4zslok3d7ld2mu0&rid=100.webp&ct=g"
+                alt="Run Gif"/>
+            <button onClick={connectWallet} className="cta-button connect-wallet-button">
+                Connect Wallet
+            </button>
+        </div>
+    );
+
+    useEffect(() => {
+        checkIfWalletIsConnected()
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="container">
+
+                <div className="header-container">
+                    <header>
+                        <div className="left">
+                            <p className="title">🐱‍👤 Changjiashuai Name Service</p>
+                            <p className="subtitle">Your immortal API on the blockchain!</p>
+                        </div>
+                    </header>
+                </div>
+
+                {!currentAccount && renderNotConnectedContainer()}
+
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo}/>
+                    <a
+                        className="footer-text"
+                        href={TWITTER_LINK}
+                        target="_blank"
+                        rel="noreferrer"
+                    >{`built with @${TWITTER_HANDLE}`}</a>
+                </div>
             </div>
-					</header>
-				</div>
-
-        <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
+        </div>
+    );
 }
 
 export default App;
